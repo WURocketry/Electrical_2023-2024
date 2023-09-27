351330283084e2239936c6a7d9ff10127b9b33d1
Leo Kaiser <leohkaiser@gmail.com>
2023-09-26 19:29:51 -0500

    Correct IMU test code

--- Payload/Tests/IMU_TEST/IMU_TEST.ino
+++ Payload/Tests/IMU_TEST/IMU_TEST.ino
@@ -1,58 +1,85 @@
-/* Test sketch for Adafruit BNO08x sensor in UART-RVC mode */
+// Basic demo for readings from Adafruit BNO08x
+#include <Adafruit_BNO08x.h>
 
-#include "Adafruit_BNO08x_RVC.h"
+// For SPI mode, we need a CS pin
+#define BNO08X_CS 10
+#define BNO08X_INT 9
 
-Adafruit_BNO08x_RVC rvc = Adafruit_BNO08x_RVC();
+// For SPI mode, we also need a RESET 
+//#define BNO08X_RESET 5
+// but not for I2C or UART
+#define BNO08X_RESET -1
 
-void setup() {
-  // Wait for serial monitor to open
+Adafruit_BNO08x  bno08x(BNO08X_RESET);
+sh2_SensorValue_t sensorValue;
+
+void setup(void) {
   Serial.begin(115200);
-  while (!Serial)
-    delay(10);
+  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("Adafruit BNO08x IMU - UART-RVC mode");
+  Serial.println("Adafruit BNO08x test!");
 
-  Serial1.begin(115200); // This is the baud rate specified by the datasheet
-  while (!Serial1)
-    delay(10);
+  // Try to initialize!
+  if (!bno08x.begin_I2C()) {
+  //if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte UART buffer!
+  //if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
+    Serial.println("Failed to find BNO08x chip");
+    while (1) { delay(10); }
+  }
+  Serial.println("BNO08x Found!");
 
-  if (!rvc.begin(&Serial1)) { // connect to the sensor over hardware serial
-    Serial.println("Could not find BNO08x!");
-    while (1)
-      delay(10);
+  for (int n = 0; n < bno08x.prodIds.numEntries; n++) {
+    Serial.print("Part ");
+    Serial.print(bno08x.prodIds.entry[n].swPartNumber);
+    Serial.print(": Version :");
+    Serial.print(bno08x.prodIds.entry[n].swVersionMajor);
+    Serial.print(".");
+    Serial.print(bno08x.prodIds.entry[n].swVersionMinor);
+    Serial.print(".");
+    Serial.print(bno08x.prodIds.entry[n].swVersionPatch);
+    Serial.print(" Build ");
+    Serial.println(bno08x.prodIds.entry[n].swBuildNumber);
   }
 
-  Serial.println("BNO08x found!");
+  setReports();
+
+  Serial.println("Reading events");
+  delay(100);
 }
 
-void loop() {
-  BNO08x_RVC_Data heading;
+// Here is where you define the sensor outputs you want to receive
+void setReports(void) {
+  Serial.println("Setting desired reports");
+  if (! bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+    Serial.println("Could not enable game vector");
+  }
+}
 
-  if (!rvc.read(&heading)) {
+
+void loop() {
+  delay(10);
+
+  if (bno08x.wasReset()) {
+    Serial.print("sensor was reset ");
+    setReports();
+  }
+  
+  if (! bno08x.getSensorEvent(&sensorValue)) {
     return;
   }
+  
+  switch (sensorValue.sensorId) {
+    
+    case SH2_GAME_ROTATION_VECTOR:
+      Serial.print("Game Rotation Vector - r: ");
+      Serial.print(sensorValue.un.gameRotationVector.real);
+      Serial.print(" i: ");
+      Serial.print(sensorValue.un.gameRotationVector.i);
+      Serial.print(" j: ");
+      Serial.print(sensorValue.un.gameRotationVector.j);
+      Serial.print(" k: ");
+      Serial.println(sensorValue.un.gameRotationVector.k);
+      break;
+  }
 
-  Serial.println();
-  Serial.println(F("---------------------------------------"));
-  Serial.println(F("Principal Axes:"));
-  Serial.println(F("---------------------------------------"));
-  Serial.print(F("Yaw: "));
-  Serial.print(heading.yaw);
-  Serial.print(F("\tPitch: "));
-  Serial.print(heading.pitch);
-  Serial.print(F("\tRoll: "));
-  Serial.println(heading.roll);
-  Serial.println(F("---------------------------------------"));
-  Serial.println(F("Acceleration"));
-  Serial.println(F("---------------------------------------"));
-  Serial.print(F("X: "));
-  Serial.print(heading.x_accel);
-  Serial.print(F("\tY: "));
-  Serial.print(heading.y_accel);
-  Serial.print(F("\tZ: "));
-  Serial.println(heading.z_accel);
-  Serial.println(F("---------------------------------------"));
-
-
-  //  delay(200);
 }
