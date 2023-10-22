73f9621b8612f04749c2f56d0e3381492e547e8c
Leo Kaiser <leohkaiser@gmail.com>
2023-10-22 13:17:49 -0500

    updated GPS method

--- Payload/main/main.ino
+++ Payload/main/main.ino
@@ -236,34 +236,21 @@ void collectDataFromGPS()
   // approximately every 2 seconds or so, print out the current stats
   if (millis() - timer > 2000) {
     timer = millis(); // reset the timer
-    Serial.print("\nTime: ");
-    if (GPS.hour < 10) { Serial.print('0'); }
-    Serial.print(GPS.hour, DEC); Serial.print(':');
-    if (GPS.minute < 10) { Serial.print('0'); }
-    Serial.print(GPS.minute, DEC); Serial.print(':');
-    if (GPS.seconds < 10) { Serial.print('0'); }
-    Serial.print(GPS.seconds, DEC); Serial.print('.');
-    if (GPS.milliseconds < 10) {
-      Serial.print("00");
-    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
-      Serial.print("0");
     }
-    Serial.println(GPS.milliseconds);
-    Serial.print("Date: ");
-    Serial.print(GPS.day, DEC); Serial.print('/');
-    Serial.print(GPS.month, DEC); Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
     if (GPS.fix) {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", ");
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+      DATA_COMPONENT_READINGS[GPS_LATITUDE] = GPS.lat;
+      DATA_COMPONENT_READINGS[GPS_LONGITUDE] = GPS.lon;
+      DATA_COMPONENT_READINGS[GPS_HOUR] = GPS.hour;
+      DATA_COMPONENT_READINGS[GPS_SPEED] = GPS.speed;
+      DATA_COMPONENT_READINGS[GPS_ALTITUDE] = GPS.altitude;
+    }
+    else {
+      DATA_COMPONENT_READINGS[GPS_LATITUDE] = -1;
+      DATA_COMPONENT_READINGS[GPS_LONGITUDE] = -1;
+      DATA_COMPONENT_READINGS[GPS_HOUR] = -1;
+      DATA_COMPONENT_READINGS[GPS_SPEED] = -1;
+      DATA_COMPONENT_READINGS[GPS_ALTITUDE] = -1;
+
     }
   }
 }
