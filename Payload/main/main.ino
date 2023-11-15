e4f3f0474371f77265db03b5bb26713805ed144e
jiwenofu <c.iwenofu@wustl.edu>
2023-11-14 19:41:33 -0600

    time testing

--- Payload/main/main.ino
+++ Payload/main/main.ino
@@ -90,6 +90,10 @@ sh2_SensorId_t reportType = SH2_ARVR_STABILIZED_RV;
 long reportIntervalUs = 5000;
 
 // BME680 Definitions 
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 //GPS Defintions
@@ -124,7 +128,7 @@ int16_t packetnum = 0;
 
 //Define timing separations for devices
 float bnoTime = 2000;
-float bmeTime = 2000;
+float bmeTime = 200;
 float gpsTime = 2000;
 float batteryTime = 2000;
 float rfTime = 1000;
@@ -212,6 +216,11 @@ void setup() {
   writeColumnHeaders(ENUM_NAMES, fullidx, filename);
   writeColumnHeaders(ENUM_NAMES, smallidx, smallFileName);
 
+  bmeTimer = millis();
+  bnoTimer = millis();
+  gpsTimer = millis();
+  batteryTimer = millis();
+  rfTimer = millis();
 
 
 }
@@ -283,9 +292,9 @@ void collectDataFromBNO() {
 // Function to collect data from BME680
 void collectDataFromBME() {
   unsigned long currentMillis = millis();
-  if (currentMillis >= bmeTime + bmeTimer) {
+  if (currentMillis >= bmeTimer) {
     bmeTimer += bmeTime;
-    if (bme.performReading()) {
+    if (bme.beginReading()) {
 
       DATA_COMPONENT_READINGS[BME_TEMPERATURE] = bme.temperature;
       DATA_COMPONENT_READINGS[BME_PRESSURE] = bme.pressure;
@@ -293,9 +302,11 @@ void collectDataFromBME() {
       DATA_COMPONENT_READINGS[BME_GAS] = bme.gas_resistance / 1000.0;
       DATA_COMPONENT_READINGS[BME_ALTITUDE] = bme.readAltitude(SEALEVELPRESSURE_HPA);
 
-    } else {
+    }
+     else {
       Serial.println("Failed to perform BME680 reading");
     }
+    bme.endReading();
   }
 }
 
