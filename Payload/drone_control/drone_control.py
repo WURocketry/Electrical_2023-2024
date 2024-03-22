1e8d6d7f393fbe8bb7d448c6b8d24798dbf784d3
Autumn Xu <autumn@wustl.edu>
2024-03-22 13:41:28 -0500

    control flow refining

--- Payload/drone_control/drone_control.py
+++ Payload/drone_control/drone_control.py
@@ -2,16 +2,51 @@ import time
 import RPi.GPIO as GPIO
 from dronekit import connect, VehicleMode
 from enum import Enum
+import board
+import busio
+import digitalio
+import adafruit_rfm9x
 # Enum for GPIO pin setup
 class Pin(Enum):
     SEPARATION_PIN = 18  # Example pin for H-bridge control
+
+class PacketStatus(Enum):
+    INITIAL = "INITIAL"
+    RSO_RECEIVED = "RSO_RECEIVED"
+    FORCE_DETACH = "EMERGENCY_DETACH"
+    FORCE_ARM = "EMERGENCY_ARM"
+    DETACHED = "DETACHED"
+    ARMED = "ARMED"
+
+status = PacketStatus.INITIAL
+# Define radio parameters.
+RADIO_FREQ_MHZ = 434.4  # Frequency of the radio in Mhz. Must match your module! Can be a value like 915.0, 433.0, etc.
+
+# Define pins connected to the chip, use these if wiring up the breakout according to the guide:
+CS = digitalio.DigitalInOut(board.CE1)
+RESET = digitalio.DigitalInOut(board.D6)
+
+# Initialize SPI bus.
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+# Initialze RFM radio
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# You can however adjust the transmit power (in dB).  The default is 13 dB but
+# high power radios like the RFM95 can go up to 23 dB:
+rfm9x.tx_power = 23
+
+
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)  # BCM numbering double check that
 GPIO.setup(Pin.SEPARATION_PIN.value, GPIO.OUT)
 # Connect to the Vehicle still need code to make sure connection
 print("Connecting to vehicle on: 'serial port here'")
-vehicle = connect('/dev/ttyAMA0', wait_ready=True, baud=57600)  # Adjust serial port & baud rate
+vehicle = connect('/dev/ttyAMA0', wait_ready=True)  # Adjust serial port & baud rate
 pause_script=False
+# fail safe
+separationCompleted = False
+
 def arm_drone_and_land():
     """
     Arms the vehicle and changes its flight mode to LAND.
@@ -23,6 +58,7 @@ def arm_drone_and_land():
     print("Turning on Land Mode")
     vehicle.mode = VehicleMode("LAND")
     print("Drone is armed and in LAND mode")
+
 def detach_drone(seconds):
     """
     Activates the GPIO pin to control the H bridge for drone detach.
@@ -33,16 +69,46 @@ def detach_drone(seconds):
     GPIO.output(Pin.DETACH_PIN.value, GPIO.LOW)
     print("Drone separated")
     
-def process_packets():
+def process_packets(status):
     """
     Placeholder for processing received packets. Implement as needed.
     """
-    pass
+    packet = rfm9x.receive() # blocking code of execution -- need confirmation from official documentation
+    # Optionally change the receive timeout from its default of 0.5 seconds:
+    # packet = rfm9x.receive(timeout=5.0)
+    # If no packet was received during the timeout then None is returned.
+    if packet is None: # it would be the else branch only 
+        # Packet has not been received
+	    #LED.value = False
+        print("Received nothing! Listening again...")
+    else:
+        # Received a packet!
+        # LED.value = True
+        # Print out the raw bytes of the packet:
+        print("Received (raw bytes): {0}".format(packet))
+        # And decode to ASCII text and print it too.  Note that you always
+        # receive raw bytes and need to convert to a text format like ASCII
+        # if you intend to do string processing on your data.  Make sure the
+        # sending side is sending ASCII data before you try to decode!
+        packet_text = str(packet, "ascii")
+        print("Received (ASCII): {0}".format(packet_text))
+        if packet_text == "RSO":
+            status = PacketStatus.RSO_RECEIVED
+        if packet_text == "DETACH":
+            status = PacketStatus.FORCE_DETACH
+        if packet_text == "ARM":
+            status = PacketStatus.FORCE_ARM
+        # Also read the RSSI (signal strength) of the last received message and
+        # print it.
+        rssi = rfm9x.last_rssi
+        print("Received signal strength: {0} dB".format(rssi))
+    return status
 
-
+# test this function later, let's make sure the radio stuffs works 
 @vehicle.on_attribute('last_heartbeat')
 def listener(self, attr_name, value):
     global pause_script
+    # 1 sec 
     if value > 1 and not pause_script:
         print("Pausing script due to bad link")
         pause_script=True
@@ -50,7 +116,7 @@ def listener(self, attr_name, value):
         pause_script=False
         print("Un-pausing script")
 
-def main():
+def main(status):
     while True:
         #check if we are connected, and if not we are going to reconnect
         #transmit the vehicle status do a delta time 2 seconds
@@ -58,24 +124,34 @@ def main():
         print("Battery: %s" % vehicle.mode.name)
         print("Mode: %s" % vehicle.mode.name)
         print("Altitude: %s" % vehicle.location.global_relative_frame.alt)
+        print("Status: %s" % status)
         #this will check if we have RSO permission.
-        packet = process_packets()
+        status = process_packets(status)
+        '''
         if packet == "rso":
             rsoPermission = True
+        '''
         #if we have rso, and have not  separated yet, do the process.
-        if rsoPermission and vehicle.location.global_relative_frame.alt < 122 and not separationCompleted:
+        if status == PacketStatus.RSO_RECEIVED and vehicle.location.global_relative_frame.alt < 122 and not separationCompleted:
             detach_drone()
+            status = PacketStatus.DETACHED
             arm_drone_and_land()
             separationCompleted = True
+            status = PacketStatus.ARMED
+    
         #Backup if something goes wrong, force arm or force separate.
-        if packet == "detach":
+        if status == PacketStatus.FORCE_DETACH:
             detach_drone()
-        if packet == "arm":
+            status = PacketStatus.DETACHED
+        if status == PacketStatus.FORCE_ARM:
             arm_drone_and_land()
+            status = PacketStatus.ARMED
+    
 
 if __name__ == "__main__":
     try:
-        main()
+        status = PacketStatus.INITIAL
+        main(status)
     except KeyboardInterrupt:
         print("Program terminated by user")
     finally:
