0662a7c6fbc95859826fa3118d589cfe2eaa53d4
MackLaRosa <larosa.m@wustl.edu>
2024-03-29 14:43:44 -0500

    poop

--- Avionics/execute_video.sh
+++ Avionics/execute_video.sh
@@ -2,8 +2,11 @@
 
 count=0
 
+flightNum=$(cat /home/rocketrycamera/Desktop/Flight_Counter.txt)
+mkdir /home/rocketrycamera/Desktop/Flight_Number_$flightNum
+
 while [ true ]; do
 	count=$((count+1))
 	#echo ~/Desktop/elsberry_full_2_10_24/$count.h264
-	libcamera-vid --width 1280 --height 720 -t 300000 --framerate 50 -o /home/rocketrycamera/Desktop/elsberry_full_2_10_24/$count.h264
+	libcamera-vid --width 1280 --height 720 -t 420000 --framerate 30 -o /home/rocketrycamera/Desktop/Flight_Number_$flightNum/$count.h264
 done
