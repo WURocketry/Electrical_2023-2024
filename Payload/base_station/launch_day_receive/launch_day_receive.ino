3897466b229daf3c655f8196d080805fa3413290
Autumn Xu <autumn@wustl.edu>
2024-03-29 17:37:21 -0500

    updated alaunch day receive

--- Payload/base_station/launch_day_receive/launch_day_receive.ino
+++ Payload/base_station/launch_day_receive/launch_day_receive.ino
@@ -5,49 +5,19 @@
 #define RFM95_IRQ   21  // Interrupt pin, connected to DIO0
 #define RFM95_GPIO  22  // Additional GPIO, connected to DIO1
 SX1276 radio = new Module(RFM95_CS, RFM95_IRQ, RFM95_RST, RFM95_GPIO);
-/*
-const char* ENUM_NAMES[] = {
-//  "BNO_YAW",
-//  "BNO_PITCH",
-//  "BNO_ROLL",
-  "BNO_XACCEL",
-  "BNO_YACCEL",
-  "BNO_ZACCEL",
-//  "SECONDS_SINCE_ON",
-  "BME_TEMPERATURE",
-//  "BME_PRESSURE",
-//  "BME_HUMIDITY",
-//  "BME_GAS",
-  "BME_ALTITUDE",
- // "GPS_HOUR",
- // "GPS_MINUTE",
- // "GPS_SPEED",
-  "GPS_LATITUDE",
-  "GPS_LONGITUDE",
-//  "GPS_ALTITUDE",
-  "BATTERY_PERCENT",
-//  "BATTERY_VOLTAGE",
-//  "BATTERY_DISCHARGE_RATE_%_PER_HOUR",
-};
-*/
 const char* ENUM_NAMES[] = {
   "XACCEL_BNO",
   "YACCEL_BNO",
   "ZACCEL_BNO",
   "TEMPERATURE_BME",
   "ALTITUDE_BME",
-  "LATITUDE_GPS",
-  "LONGITUDE_GPS",
-  "PERCENT_BATERRY",
-
-};  
-//LIVE_RADIO_SIZE,
-
+  "VOLATILE_COMPONENT"
+};
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   float transmitPower = 20;
-  float currentFrequency = 916.23;
+  float currentFrequency = 915;
   //LoRa Setup
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
@@ -92,22 +62,18 @@ void loop() {
     Serial.println("Received message: '");
     //Serial.print(str);
     //Serial.println("'");
-    
      // Split the received data using the comma as a delimiter
     char *token = strtok(const_cast<char*>(str.c_str()), ",");
     int index = 0;
-
-    while (token != NULL && index < 9) {
+    while (token != NULL && index < 6) {
       // Print the component name and its corresponding value
       Serial.print(ENUM_NAMES[index]);
       Serial.print(", Value: ");
       Serial.println(token);
-
       // Move on to the next token
       token = strtok(NULL, ",");
       index++;
     }
-    
   } else if (state != RADIOLIB_ERR_RX_TIMEOUT) {
     // An error occurred while attempting to receive a message.
     Serial.print("Receive failed, error code: ");
